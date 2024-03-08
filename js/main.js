3c347ea6b556346030734915da60dd3d1c0bc38d
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-03-08 21:49:49 +0330

    slider tetsimonial section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
-const navTriggerBtn = document.querySelector("#nav-tigger-btn")
-const navMenu = document.querySelector("#nav-menu")
+const navTriggerBtn = document.querySelector("#nav-tigger-btn");
+const navMenu = document.querySelector("#nav-menu");
 
-navTriggerBtn.addEventListener("click",()=>{
-    navMenu.classList.toggle("nav-open")
-})
+navTriggerBtn.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-open");
+});
+
+// swiper
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  slidesPerView: 3,
+  spaceBetween: 20,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    960: {
+        slidesPerView: 22,
+      },
+      1200: {
+        slidesPerView: 3,
+      },
+  },
+});
