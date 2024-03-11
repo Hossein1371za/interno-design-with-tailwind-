8e7a25bd137a13522cda5371446289d5c9dd71e5
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-03-11 23:51:09 +0330

    animation scrollreveal

--- js/main.js
+++ js/main.js
@@ -26,3 +26,31 @@ const swiper = new Swiper(".swiper", {
       },
   },
 });
+
+const sr = ScrollReveal({
+  origin:"bottom",
+  distance: "60px",
+  duration:3000,
+  delay:600,
+  reset:"true",
+})
+
+sr.reveal('.hero__text',{origin:"top"})
+
+
+sr.reveal('.steps__step',{distance:"100px",interval:100})
+
+
+sr.reveal('.about__text',{origin:"right"})
+sr.reveal('.about__img',{origin:"left",delay:800})
+
+
+sr.reveal('.testimonial__bg',{delay:800})
+sr.reveal('.testimonial__title')
+sr.reveal('.testimonial__slider',{delay:1000})
+
+
+sr.reveal('.brands__image',{delay:600,distance:"100px",interval:100})
+
+
+ 
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,6 @@ module.exports = {
     },
     backgroundImage: {
       hero: "url(/assets/hero/bg.jpg)",
-      grid: "url(/assets/grid.png)",
     },
     extend: {
       colors: {
